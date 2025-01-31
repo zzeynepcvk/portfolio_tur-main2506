795e18cd7ea40bffb7714acc33ea927b775ae8da
zzeynepcvk <210541009@firat.edu.tr>
2025-01-31 22:41:18 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# Import
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+# İçerik sayfasını çalıştırma
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Dinamik beceriler
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_discord = request.form.get('button_discord')
+    button_html = request.form.get('button_html')
+    return render_template('index.html',
+                           button_python=button_python, 
+                           button_discord=button_discord,
+                           button_html=button_html)
+    
+
+    
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
